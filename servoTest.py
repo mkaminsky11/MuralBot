96a4b488e73f38e28204dfd02f8c71d36e80f4a6
mkaminsky11 <mkaminsky11@gmail.com>
2017-02-25 12:47:21 -0500

    more tests

--- servoTest.py
+++ servoTest.py
@@ -8,7 +8,16 @@ gpio_num = 4 # where connected
 pi = pigpio.pi()
 
 if not pi.connected:
+	print("exit")
 	exit()
 else:
 	# all good, keep going
 	pi.set_servo_pulsewidth(gpio_num, 1000)
+	time.sleep(5)
+	pi.set_servo_pulsewidth(gpio_num, 2000)
+	time.sleep(5)
+	pi.set_servo_pulsewidth(gpio_num, 1500)
+
+# 1000 = fast one way
+# 1500 = stop
+# 2000 = fast the other way
