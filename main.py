d80365850f82cffc4578012e544b4a7b0e162c86
mkaminsky11 <mkaminsky11@gmail.com>
2017-02-26 04:46:48 +0000

    push

--- main.py
+++ main.py
@@ -9,6 +9,63 @@ if not pi.connected:
 	exit()
 # all good, keep going
 
+turns = 0
+t = 0
+goalTime = 0
+
+def goStr(tt):
+	global goalTime
+	global state
+	global startTime
+	state = 1
+	startTime = time.time()
+	s(config.d1_pin, 1172)
+	s(config.d2_pin, 2500)
+	goalTime = tt
+
+def turnL(tt):
+	global goalTime
+	global state
+	global startTime	
+	state = 2
+	startTime = time.time()
+	s(config.d1_pin, 1771)
+	s(config.d2_pin, 2500)
+	goalTime = tt
+
+def turnR(tt):
+	global goalTime
+	global state
+	global startTime
+
+	state = 2
+	startTime = time.time()
+	s(config.d1_pin, 700)
+	s(config.d2_pin, 1000)
+	goalTime = tt
+
+def corr(tt):
+	global goalTime
+	global state
+	global startTime
+	state  = 3
+	startTime = time.time()        
+	s(config.d1_pin, 1172)
+	s(config.d2_pin, 2500)
+	goalTime = tt
+
+def dn():
+	global goalTime
+	global state
+	global t
+	global started
+
+	t = 0
+	state = 0
+	started = False
+	stopAll()
+	
+
 def s(pin, state):
 	pi.set_servo_pulsewidth(pin, state)
 
@@ -19,35 +76,53 @@ def stopAll():
 ok = True
 started = False
 stopped = False
+
+lastCamTime = time.time()
+startTime = time.time()
+state = 0 # 0 = none 1 = str 2 = turn 3 = correct
+
+
+#stopAll()
+
 while(ok):
 	pressed = pi.read(config.b_pin) == 0
-	#print(pi.read(config.b_pin))
 	if pressed:
 		if not started:
 			started = True
-			
-			#
-			# STRAIGHT
-			#
+			t = 1
+			#goStr(2)
+			print("s1")
+			turnR(1.41)
+			print("s2")
+	if time.time() - startTime > goalTime:
+		if state == 1:
+			# done going straight, start turning
+			turnR(1.41)
+			print("turning")
 
-			#s(config.d1_pin, 1171)
-			#s(config.d2_pin, 2500)
-			#time.sleep(4)
-			
-			# LEFT
-			s(config.d1_pin, 1771)
-			s(config.d2_pin, 2500)
-			time.sleep(1.42)
+			# check to see if should stop
+			t = t + 1
+			if t == 2:
+				# should stop here
+				dn()
+				print("ending")
+		elif state == 2:
+			# stop turning, start correcting
+			corr(0.79)
+			print("correcting")
 
-			started = False
-			stopAll()
-			#ok = False
-		#else:
-		#	stopAll()
-		#	ok = False
+		elif state == 3:
+			dn()
+			# stop correcting, start going straight
+			#goStr(2)
+			print("straight")
+	
+
+
 pi.stop()
 
 def exit_handler():
+	stopAll()
 	print('My application is ending!')
 	
 atexit.register(exit_handler)
